a39a34273a063cd51b1e8b0ff6cff689e02ea22b
Joseph Yakubu <Josephstevenyakubu@gmail.com>
2021-10-06 14:00:13 +0100

    If applied, this commit will have declared and initialized an array with objects
    Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-alert('Hello world');
 
-let  favoriteFood = "Fried Rice";
 
-document.write(favoriteFood)
+//Declaring empty pokemonList Array
+let pokemonList = [];
+
+//Initializing pokemonList Array with Objects
+pokemonList =
+[
+    {name: 'bulbasaur', height: 0.7, type: ['grass','poison']},
+    {name: 'charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'pikachu', height: 0.4, type: ['electric']},
+    {name: 'gastly', height: 1.3, type: ['ghost','poison']},
+    {name: 'nidoking', height: 1.4, type: ['ground','poison']},
+];
