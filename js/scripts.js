21e2c0fa38f035b895fc07bbc3c3167dfe42677d
Joseph Yakubu <Josephstevenyakubu@gmail.com>
2021-10-07 02:54:46 +0100

    Task 1.3
    If applied, this commit creates a loop to traverse through an array of Pokémon objects and display more information for the tallest Pokémon
    
    Creates a for loop to iterate through the pokemon list.
    Added a conditional within the loop to display the pokemon with a height

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,24 @@ let pokemonList = [];
 //Initializing pokemonList Array with Objects
 pokemonList =
 [
-    {name: 'bulbasaur', height: 0.7, type: ['grass','poison']},
-    {name: 'charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'pikachu', height: 0.4, type: ['electric']},
-    {name: 'gastly', height: 1.3, type: ['ghost','poison']},
-    {name: 'nidoking', height: 1.4, type: ['ground','poison']},
+    {name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+    {name: 'Pikachu', height: 0.4, type: ['electric']},
+    {name: 'Gastly', height: 1.3, type: ['ghost','poison']},
+    {name: 'Nidoking', height: 1.4, type: ['ground','poison']},
 ];
+
+//For loop to traverse through all the pokemon in the array
+for(let i=0; i < pokemonList.length; i++)
+{
+  //Conditional statement to check size of pokemon and display information through DOM
+  if (pokemonList[i].height > 1.5)
+  {
+    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) - Wow, that's BIG! <br>`);
+  }
+  else
+  {
+    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) <br>`);
+  }
+  document.write("<br>");
+}
