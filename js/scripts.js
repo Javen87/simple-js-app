7f21e51abb7942a90fd928fc5938d486cd653677
Joseph Yakubu <Josephstevenyakubu@gmail.com>
2021-10-13 23:09:04 +0100

    Task 1.5
    If applied, this commit would have implemented encapsulation and Immediately Invoked Functional Expression

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,59 @@
-
-
-//Declaring empty pokemonList Array
-let pokemonList = [];
-
-//Initializing pokemonList Array with Objects
-pokemonList =
-[
+//IIFE created
+let pokemonRepository = (function () {
+  //Manaually filling pokemon array with pokemon objects
+  let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
     {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
     {name: 'Pikachu', height: 0.4, type: ['electric']},
     {name: 'Gastly', height: 1.3, type: ['ghost','poison']},
-    {name: 'Nidoking', height: 1.4, type: ['ground','poison']},
-];
+    {name: 'Nidoking', height: 1.4, type: ['ground','poison']}
+  ];
 
-//For loop to traverse through all the pokemon in the array
-for(let i=0; i < pokemonList.length; i++)
+  //Function to add pokemon object to Array of Pokemon
+  function add(pokemon){
+    //Conditional statement to ensure its a pokemon object
+    if (typeof pokemon === 'object')
+    {
+      pokemonList.push(pokemon);
+    }
+    else
+    {
+      document.write('Not a Pokemmon Object');
+    }
+  }
+  function findByName(pokemonName){
+    let filterResult = pokemonList.filter(pokemon => pokemon.name === pokemonName);
+    console.log(`We found the Pokemon: ${filterResult[0].name}`);
+  }
+
+  //Function to return all pokemon objects in the Array
+  function getAll(){
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    findByName: findByName,
+    getAll: getAll
+  }
+})();
+
+//Implementation of ForEach method to display all pokemon objects
+pokemonRepository.getAll().forEach(function(pokemon)
 {
-  //Conditional statement to check size of pokemon and display information through DOM
-  if (pokemonList[i].height > 1.5)
+  //Conditional statement to ensure an adjective is printed for the biggest pokemon object
+  if (pokemon.height > 1.5)
   {
-    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) - Wow, that's BIG! <br>`);
+    document.write(`${pokemon.name} (Height: ${pokemon.height}) - Wow, that's BIG! <br>`);
   }
   else
   {
-    document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) <br>`);
+    document.write(`${pokemon.name} (Height: ${pokemon.height}) <br>`);
   }
   document.write("<br>");
-}
+});
+
+
+//
+
+pokemonRepository.findByName('Pikachu');
